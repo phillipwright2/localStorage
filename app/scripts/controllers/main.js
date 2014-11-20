09f0688adba96834122e8c7af60410ca0b992c86
Phillip Wright <bootsie8917@gmail.com>
2014-11-19 22:03:45 -0800

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = [
+      'get fat',
+      'get skinny',
+      'get healthy'
+    ];
+  });
